e0abcd8bc17d221f239a93837e14eb5bc5d1b751
myykel <52496877+myykel@users.noreply.github.com>
2023-01-28 15:56:33 +0100

    Fixed an error in task 1 code

--- Task_1/Task1.js
+++ Task_1/Task1.js
@@ -13,9 +13,3 @@ function smallerElementsToRight(arr) {
   }
   
   console.log(smallerElementsToRight([5, 9, 7, 4, 2])); 
-
-
-  // This function takes in an array of integers as input and uses nested loops to iterate 
-  //through the array,
-  //counting the number of elements to the right that are smaller than the current element. 
-  //The result is pushed into a new array and returned.
